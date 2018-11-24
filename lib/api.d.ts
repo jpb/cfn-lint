977678c4670c743a3b00a23eb84a4fbe11fc95b5
James Brennan <james@jamesbrennan.ca>
2018-11-24 00:40:06 +0000

    npm run build

---
+++ lib/api.d.ts
@@ -0,0 +1,31 @@
+import validator = require('./validator');
+export interface APIValidationOptions {
+    parameters: {
+        [parameterName: string]: any;
+    };
+    pseudoParameters: {
+        [pseudoParameterName: string]: any;
+    };
+}
+export declare type ValidationOptions = APIValidationOptions & validator.ValidateOptions;
+export declare type ValidationResult = validator.ErrorObject;
+/**
+ * Synchronously validates a CloudFormation yaml or json file.
+ * @param fileName
+ * @param options
+ */
+export declare function validateFile(fileName: string, options?: Partial<ValidationOptions>): ValidationResult;
+/**
+ * Synchronously validates a CloudFormation yaml or json string.
+ * @param contents
+ * @param filename
+ * @param options
+ */
+export declare function validateString(contents: string, filename: string, options?: Partial<ValidationOptions>): ValidationResult;
+/**
+ * Synchronously validates an object. The object should be what you
+ * get from JSON.parse-ing or yaml.load-ing a CloudFormation template.
+ * @param objectToValidate
+ * @param options
+ */
+export declare function validateJsonObject(objectToValidate: any, options?: Partial<ValidationOptions>): ValidationResult;
---
+++ lib/parser.d.ts
@@ -0,0 +1,2 @@
+export declare function openFile(path: string): any;
+export declare function openString(contents: string, filename: string): any;
